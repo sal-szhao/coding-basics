22ee8a9e36d92f0ab583e08e8408a2880e8ef557
Sal-bug <66895728+Sal-bug@users.noreply.github.com>
2023-06-12 17:40:38 +0800

    minor fixes

--- watchlist-codes/wtf-basics/app.py
+++ watchlist-codes/wtf-basics/app.py
@@ -11,10 +11,9 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 
 class LoginForm(FlaskForm):
     email = StringField(u'邮箱', validators=[
-                DataRequired(message= u'邮箱不能为空'), Length(1, 64),
-                Email(message= u'请输入有效的邮箱地址，比如：username@domain.com')])
-    password = PasswordField(u'密码', 
-                  validators=[DataRequired(message= u'密码不能为空')])
+            DataRequired(message= u'邮箱不能为空'), Length(1, 64),
+            Email(message= u'请输入有效的邮箱地址，比如：username@domain.com')])
+    password = PasswordField(u'密码', validators=[DataRequired(message= u'密码不能为空')])
     submit = SubmitField(u'登录')
 
 
