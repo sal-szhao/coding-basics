506cbbe95ce8e6ff9754f9fc97c9a4af04eb7d41
sal-bug <spongebobandpants@163.com>
2023-06-12 09:05:12 -0400

    fix bootstrap and displaying error messages

--- watchlist-codes/wtf-basics/app.py
+++ watchlist-codes/wtf-basics/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
@@ -6,14 +7,21 @@ from wtforms.validators import DataRequired, Length, Email
 
 import os
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 
+bootstrap = Bootstrap(app)
+
+
 class LoginForm(FlaskForm):
     email = StringField(u'邮箱', validators=[
-            DataRequired(message= u'邮箱不能为空'), Length(1, 64),
-            Email(message= u'请输入有效的邮箱地址，比如：username@domain.com')])
-    password = PasswordField(u'密码', validators=[DataRequired(message= u'密码不能为空')])
+                DataRequired(message= u'邮箱不能为空'), 
+                Length(1, 64),
+                Email(message= u'请输入有效的邮箱地址, 比如:username@domain.com')])
+    password = PasswordField(u'密码', validators=[  
+                DataRequired(message= u'密码不能为空'),
+                Length(5, 18)])
     submit = SubmitField(u'登录')
 
 
@@ -26,5 +34,6 @@ def hello():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect(url_for('hello'))
+        return 'success'
+    
     return render_template('login.html', form=form)
