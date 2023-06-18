829a8f01834f7726c86866524d4643f4f031dfae
Sal <spongebobandpants@163.com>
2023-06-18 21:25:22 +0800

    minor

--- fastapi-codes/fastapi-database/sqlapp/main.py
+++ fastapi-codes/fastapi-database/sqlapp/main.py
@@ -57,5 +57,6 @@ def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return items
 
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
