b856cb2228cb2d36e539175706d2b995d33fc4d2
Sal-bug <66895728+Sal-bug@users.noreply.github.com>
2023-06-19 17:32:26 +0800

    add apirouter and async

---
+++ fastapi-codes/fastapi-apirouter/app/test_main.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+from .main import app
+
+client = TestClient(app)
+
+def test_read_users():
+    response = client.get('/users/?token=jessica')
+    assert response.status_code == 200
+    assert {"username": "Rick"} in response.json()
+    assert {"username": "Morty"} in response.json()
+
+def test_update_items():
+    response = client.put(
+        '/items/plumbus?token=jessica', 
+        headers={"X-Token": "fake-super-secret-token"},
+        )
+    assert response.status_code == 200
+    assert response.json() == {"item_id": "plumbus", "name": "The great Plumbus"}
---
+++ fastapi-codes/fastapi-async/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+import time
+import asyncio
+
+app = FastAPI()
+
+async def my_func_1():
+    """
+    my func 1
+    """
+    print('Func1 started..!!')
+    await asyncio.sleep(5)
+    print('Func1 ended..!!')
+
+    return 'a..!!'
+
+async def my_func_2():
+    """
+    my func 2
+    """
+    print('Func2 started..!!')
+    await asyncio.sleep(5)
+    print('Func2 ended..!!')
+
+    return 'b..!!'
+
+@app.get("/home")
+async def root():
+    """
+    my home route
+    """
+    start = time.time()
+    futures = [my_func_1(), my_func_2()]
+    a,b = await asyncio.gather(*futures)
+    end = time.time()
+    print('It took {} seconds to finish execution.'.format(round(end-start)))
+
+    return {
+        'a': a,
+        'b': b
+    }
+
+
