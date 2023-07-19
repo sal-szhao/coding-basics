69c06fb041806390c7b1fb1fbca1e79a665dfbba
Sal-bug <66895728+Sal-bug@users.noreply.github.com>
2023-07-19 17:25:03 +0800

    add uvicorn code

---
+++ fastapi-codes/fastapi-basic/customapp.py
@@ -0,0 +1,26 @@
+import asyncio
+
+# Defined app should expose an async callable which takes three arguments:
+# scope, receive, send.
+async def app(scope, receive, send):
+    """
+    Send a slowly streaming HTTP response back to the client.
+    """
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ]
+    })
+    for chunk in [b'Hello', b', ', b'world!']:
+        await send({
+            'type': 'http.response.body',
+            'body': chunk,
+            'more_body': True
+        })
+        await asyncio.sleep(1)
+    await send({
+        'type': 'http.response.body',
+        'body': b'\nabaaba',
+    })
